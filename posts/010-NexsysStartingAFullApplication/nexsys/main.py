3095189637447f86f786c0fd1d58ac806a6aec84
Mark Streatfield <mstreatfield@gmail.com>
2011-11-20 13:32:47 +1100

    + Code for Tutorial 10 - Nexsys, Starting a Full Application

---
+++ posts/010-NexsysStartingAFullApplication/nexsys/main.py
@@ -0,0 +1,46 @@
+""" 
+Main entry point to the nexsys application.
+"""
+
+# define authorship information
+__authors__     = ["Mark Streatfield"]
+__author__      = ",".join(__authors__)
+__credits__     = ["Eric Hulser"]
+__copyright__   = "Copyright (c) 2011"
+__license__     = "GPL"
+
+# maintanence information
+__maintainer__  = "Mark Streatfield"
+__email__       = "mark.streatfield@gmail.com"
+
+from PyQt4 import QtGui
+
+def main(argv=None):
+    """
+    Creates the main window for the nexsys application and begins the
+    QApplication if necessary.
+    """
+    app = None
+
+    if argv is None:
+        argv = []
+
+    # Create the application if necessary.
+    if not QtGui.QApplication.instance():
+        app = QtGui.QApplication(argv)
+        app.setStyle("plastique")
+
+    # Create the main window.
+    QtGui.QMessageBox.information(None, "Stub", "Create the Main Window!")
+
+    # Run the application if necessary.
+    if app:
+        return app.exec_()
+    
+    # No errors since we're not running our own event loop.
+    return 0
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(main(sys.argv))
+
---
+++ posts/010-NexsysStartingAFullApplication/nexsys/settings.py
@@ -0,0 +1,15 @@
+""" 
+Module to contain all global settings for the nexsys application.
+"""
+
+# define authorship information
+__authors__     = ["Mark Streatfield"]
+__author__      = ",".join(__authors__)
+__credits__     = ["Eric Hulser"]
+__copyright__   = "Copyright (c) 2011"
+__license__     = "GPL"
+
+# maintanence information
+__maintainer__  = "Mark Streatfield"
+__email__       = "mstreatfield@gmail.com"
+
