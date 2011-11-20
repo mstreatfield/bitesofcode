fbc5daf52370ea8ea23c51ee457dffd12ca2ef7c
Mark Streatfield <mstreatfield@gmail.com>
2011-11-20 13:35:51 +1100

    + Code for Tutorial 14 - Nexsys, Main Window UI.

---
+++ posts/014-NexsysMainWindowUI/nexsys/gui/__init__.py
@@ -0,0 +1,15 @@
+""" 
+Sub-package containing all user interface components for nexsys.
+"""
+
+# define authorship information
+__authors__     = ["Mark Streatfield"]
+__author__      = ",".join(__authors__)
+__credits__     = ["Eric Hulser"]
+__copyright__   = "Copyright (c) 2011"
+__license__     = "GPL"
+
+# maintanence information
+__maintainer__  = "Mark Streatfield"
+__email__       = "mstreatfield@gmail.com"
+
